027c229523f6f230410271bec95697c3c6b2390c
Ericles922 <eric-les@hotmail.com>
2024-07-10 10:44:14 -0300

    versão funcional

--- Conversor de moedas/conversor_de_moedas/lib/App/HomePageView/HomePageView.dart
+++ Conversor de moedas/conversor_de_moedas/lib/App/HomePageView/HomePageView.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:conversor_de_moedas/App/Models/models.dart';
 import 'package:conversor_de_moedas/repositories/repositorie_moedas_monetarias.dart';
 import 'package:flutter/material.dart';
@@ -12,18 +10,14 @@ class HomePageView extends StatefulWidget {
 class _HomePageViewState extends State<HomePageView> {
   RepositorieMoedasMonetarias repository = RepositorieMoedasMonetarias();
 
+  int positionDropdown = 1;
+
   @override
   Widget build(BuildContext context) {
     List<MoedasMonetarias> moedas = repository.index();
-    //List _controllerValue = Index['Real','Dolar','Libra','Euro'];
-
-
 
     return Scaffold(
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: SingleChildScrollView(
+      body: SingleChildScrollView(
           child: Column(
             children: <Widget>[
               SizedBox(
@@ -33,44 +27,68 @@ class _HomePageViewState extends State<HomePageView> {
                   height: 200,
                 ),
               ),
-              //ListView() Estudar Depois **Não esquecer!!!!
-              SizedBox(
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: DropdownButton(
-                        items: [
-                          DropdownMenuItem(
-                              value: 1,
-                              child: Text("Real",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ))),
-                          DropdownMenuItem(value: 2, child: Text("Dolar")),
-                          DropdownMenuItem(value: 3, child: Text("Euro")),
-                          DropdownMenuItem(value: 4, child: Text("Libra")),
-                          DropdownMenuItem(value: 5, child: Text("Bitcoin"))
-                        ],
-                        onChanged: (value) {
-                          setState(() => value.toString()
-                      );}, 
-                        padding: const EdgeInsets.only(top: 16),
-                      ),
-                    ),
-                    const Expanded(flex: 2, child: TextField())
-                  ],
+              /*  Expanded(
+                child: ListView.builder(
+                  itemCount: moedas.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      title: Text(moedas.elementAt(index).name),
+                    );
+                  },
                 ),
+              ),
+              */
+              //ListView() Estudar Depois **Não esquecer!!!!
+              Row(
+                children: [
+                  Container(
+                    alignment: AlignmentDirectional(5, 10),
+                    width: 100,
+                    height: 50,
+                    child: DropdownButton(icon: Icon(Icons.arrow_drop_down,color: Colors.amber),
+                      dropdownColor: Color.fromRGBO(59, 63, 66, 1),
+                      borderRadius: BorderRadius.all(Radius.circular(0)),
+                      value: positionDropdown,
+                      items: [
+                        DropdownMenuItem(
+                            value: 1,
+                            child: Text("Real",
+                                style: TextStyle(color: Colors.amber))),
+                        DropdownMenuItem(
+                            value: 2,
+                            child: Text("Dolar",
+                                style: TextStyle(color: Colors.amber))),
+                        DropdownMenuItem(
+                            value: 3,
+                            child: Text("Euro",
+                                style: TextStyle(color: Colors.amber))),
+                        DropdownMenuItem(
+                            value: 4,
+                            child: Text("Libra",
+                                style: TextStyle(color: Colors.amber))),
+                        DropdownMenuItem(
+                            value: 5,
+                            child: Text("Bitcoin",
+                                style: TextStyle(color: Colors.amber)))
+                      ],
+                      onChanged: (value) {
+                        setState(() => positionDropdown = value ?? 1);
+                      },
+                      padding: const EdgeInsets.only(top: 20),
+                    ),
+                  ), //TextField(decoration: InputDecoration(counterText: 'Valor'),)
+                ],
               ),
               ElevatedButton(
                 onPressed: () {},
-                child: const Text("CONVERTER"),
+                child: const Text("CONVERTER",
+                ),
+               // style: ButtonStyle(backgroundColor: ),
               ),
             ],
           ),
-        ),
-      ),
-      backgroundColor: const Color.fromRGBO(59, 63, 66, 1),
-    );
+        ),backgroundColor: const Color.fromRGBO(59, 63, 66, 1),
+      );
+      
   }
 }
